d957da3f6fbd3d0e537c6bafb3ad47c687531cfa
caobandoc <obandocarlosandres@gmail.com>
2020-12-10 10:30:28 -0500

    Coreccion ejercicio 3 Parte 1

--- src/main/java/com/clearminds/impl/PersonaManager.java
+++ src/main/java/com/clearminds/impl/PersonaManager.java
@@ -8,12 +8,11 @@ public class PersonaManager {
 	private ServicioPersona serv;
 
 	public PersonaManager() {
-		serv = new ServicioPersonaBDD();
+		serv = new ServicioPersonaArchivos();
 	}
 	
 	public void insertarPersona(Persona persona) {
-		ServicioPersona aux = new ServicioPersonaArchivos();
-		aux.insertar(persona);
+		serv.insertar(persona);
 	}
 	
 	public void actualizarPersona(Persona persona) {
