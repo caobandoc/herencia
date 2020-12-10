9ab465bb845a6c7b9cb44fe41e43525fa31d7740
caobandoc <obandocarlosandres@gmail.com>
2020-12-10 10:58:43 -0500

    Agregando Class

--- src/main/java/com/clearminds/impl/PersonaManager.java
+++ src/main/java/com/clearminds/impl/PersonaManager.java
@@ -1,26 +1,35 @@
 package com.clearminds.impl;
 
+import com.clearminds.excepciones.InstanceException;
 import com.clearminds.interfaces.ServicioPersona;
 import com.clearminds.model.Persona;
 
 public class PersonaManager {
-	
+
 	private ServicioPersona serv;
 
-	public PersonaManager() {
-		serv = new ServicioPersonaArchivos();
+	public PersonaManager() throws InstanceException {
+		// serv = new ServicioPersonaArchivos();
+		Class<?> clase = null;
+		try {
+			clase = Class.forName("com.clearminds.impl.ServicioPersonaBDD");
+			serv = (ServicioPersona) clase.newInstance();
+		} catch (Exception e) {
+			throw new InstanceException("Error al obtener una instancia de ServicioPersona");
+		}
+		
 	}
-	
+
 	public void insertarPersona(Persona persona) {
 		serv.insertar(persona);
 	}
-	
+
 	public void actualizarPersona(Persona persona) {
 		serv.actualizar(persona);
 	}
-	
+
 	public void eliminarPersona(Persona persona) {
 		serv.eliminar(persona);
 	}
-	
+
 }
--- src/main/java/com/clearminds/impl/TestManager.java
+++ src/main/java/com/clearminds/impl/TestManager.java
@@ -1,11 +1,17 @@
 package com.clearminds.impl;
 
+import com.clearminds.excepciones.InstanceException;
 import com.clearminds.model.Persona;
 
 public class TestManager {
 
 	public static void main(String[] args) {
-		PersonaManager test = new PersonaManager();
+		PersonaManager test = null;
+		try {
+			test = new PersonaManager();
+		} catch (InstanceException e) {
+			e.printStackTrace();
+		}
 		Persona p = new Persona();
 		
 		test.insertarPersona(p);
