7ed9e57ea84ce4d8288be3cfe48f14fa2b5153c5
caobandoc <obandocarlosandres@gmail.com>
2020-12-10 11:21:43 -0500

    implementando class y archivo config.properties

--- src/main/java/com/clearminds/impl/PersonaManager.java
+++ src/main/java/com/clearminds/impl/PersonaManager.java
@@ -4,6 +4,10 @@ import com.clearminds.excepciones.InstanceException;
 import com.clearminds.interfaces.ServicioPersona;
 import com.clearminds.model.Persona;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class PersonaManager {
 
 	private ServicioPersona serv;
@@ -12,12 +16,37 @@ public class PersonaManager {
 		// serv = new ServicioPersonaArchivos();
 		Class<?> clase = null;
 		try {
-			clase = Class.forName("com.clearminds.impl.ServicioPersonaBDD");
+			clase = Class.forName(leerArchivo());
 			serv = (ServicioPersona) clase.newInstance();
 		} catch (Exception e) {
 			throw new InstanceException("Error al obtener una instancia de ServicioPersona");
 		}
-		
+
+	}
+
+	private static String leerArchivo() {
+		File archivo = null;
+		FileReader fr = null;
+		BufferedReader br = null;
+		String linea = null;
+		try {
+			archivo = new File("config.properties");
+			fr = new FileReader(archivo);
+			br = new BufferedReader(fr);
+			linea = br.readLine();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (null != fr) {
+					fr.close();
+				}
+
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return linea;
 	}
 
 	public void insertarPersona(Persona persona) {
